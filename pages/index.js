83c82521681755abb4833c178c66d3a110b08077
samkahchiin <samkahchiin@gmail.com>
2020-06-17 19:59:54 +0800

    [Finishes] Simple Navigations

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,13 @@
 import React from 'react';
+import Link from 'next/link';
+import Router from 'next/router';
 
-const mainPage = () => (
+const indexPage = () => (
   <div>
     <h1>Hello Main Page</h1>
+    <p>Go to <Link href='/auth'>Auth Page</Link></p>
+    <button onClick={() => Router.push('/auth')}>Go to Auth</button>
   </div>
 );
 
-export default mainPage;
+export default indexPage;
